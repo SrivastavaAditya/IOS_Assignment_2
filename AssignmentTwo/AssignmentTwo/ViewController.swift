1ccb5718f84c27593a71ba1eef7ecfcda7bd9cbe
Aditya Srivastava <aditya.srivastav@jublfood.com>
2020-07-20 13:34:07 +0530

    project creation

---
+++ AssignmentTwo/AssignmentTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentTwo
+//
+//  Created by Aditya Srivastav on 20/07/20.
+//  Copyright © 2020 Aditya Srivastav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
